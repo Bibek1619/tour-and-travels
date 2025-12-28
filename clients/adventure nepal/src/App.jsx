8b2400d791342cfacf88dc3865ab8d3e737e4b0f
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2025-12-28 19:19:08 +0545

    feat: Add route for VehicleBooking page

--- clients/adventure nepal/src/App.jsx
+++ clients/adventure nepal/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
+import VehicleBooking from "@/pages/VehicleBooking";
 // import other pages like About, Contact etc.
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/vehicle-booking" element={<VehicleBooking />} />
         {/* Add other routes below */}
         {/* <Route path="/about" element={<About />} /> */}
         {/* <Route path="/contact" element={<Contact />} /> */}
