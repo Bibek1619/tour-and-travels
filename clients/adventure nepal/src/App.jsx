3fcd77640b4d8b09c5b9c73fdb6c1d19886bb587
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2025-12-29 23:34:43 +0545

    Refactor App component for lazy loading and update index.css for font and Tailwind imports

--- clients/adventure nepal/src/App.jsx
+++ clients/adventure nepal/src/App.jsx
@@ -1,10 +1,12 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "@/components/Loader";
+import TourPackagesPage from "./pages/TourPakagesPage";
 
 // Lazy load pages
 const Home = lazy(() => import("@/pages/Home"));
 const VehicleBooking = lazy(() => import("@/pages/VehicleBooking"));
+const TourPackages = lazy(() => import("@/pages/TourPakagesPage"));
 // Add other pages as needed
 // const About = lazy(() => import("@/pages/About"));
 
@@ -16,6 +18,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/vehicle-booking" element={<VehicleBooking />} />
           {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/tours" element={<TourPackagesPage />} />
+
         </Routes>
       </Suspense>
     </Router>
