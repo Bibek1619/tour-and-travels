bfd5bcb801df1994ff0f9f7d267eac4b829ebc1a
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2025-12-30 22:54:02 +0545

    Add lazy loading for PakagesDetailsPage and update routes in App component

--- clients/adventure nepal/src/App.jsx
+++ clients/adventure nepal/src/App.jsx
@@ -2,11 +2,13 @@ import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "@/components/Loader";
 import TourPackagesPage from "./pages/TourPakagesPage";
-
+import PakagesDetailsPage from "./pages/PakagesDetailsPage";
 // Lazy load pages
 const Home = lazy(() => import("@/pages/Home"));
 const VehicleBooking = lazy(() => import("@/pages/VehicleBooking"));
 const TourPackages = lazy(() => import("@/pages/TourPakagesPage"));
+const PakagesDetails = lazy(() => import("@/pages/PakagesDetailsPage"));
+
 // Add other pages as needed
 // const About = lazy(() => import("@/pages/About"));
 
@@ -19,6 +21,7 @@ function App() {
           <Route path="/vehicle-booking" element={<VehicleBooking />} />
           {/* <Route path="/about" element={<About />} /> */}
           <Route path="/tours" element={<TourPackagesPage />} />
+          <Route path="/tours/:slug" element={<PakagesDetailsPage />} />
 
         </Routes>
       </Suspense>
