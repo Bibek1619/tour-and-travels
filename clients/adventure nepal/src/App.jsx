03d18f72ae1d3ca03f75dab5f8c979bb884be5dd
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2025-12-31 23:31:03 +0545

    Fix import statements for TourPackagesPage and PackagesDetailsPage in App component

--- clients/adventure nepal/src/App.jsx
+++ clients/adventure nepal/src/App.jsx
@@ -1,16 +1,13 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "@/components/Loader";
-import TourPackagesPage from "./pages/TourPakagesPage";
-import PakagesDetailsPage from "./pages/PakagesDetailsPage";
+
 // Lazy load pages
 const Home = lazy(() => import("@/pages/Home"));
 const VehicleBooking = lazy(() => import("@/pages/VehicleBooking"));
-const TourPackages = lazy(() => import("@/pages/TourPakagesPage"));
-const PakagesDetails = lazy(() => import("@/pages/PakagesDetailsPage"));
-
-// Add other pages as needed
-// const About = lazy(() => import("@/pages/About"));
+const TourPackagesPage = lazy(() => import("@/pages/TourPakagesPage"));
+const PakagesDetailsPage = lazy(() => import("@/pages/PakagesDetailsPage"));
+const SeatBooking = lazy(() => import("@/pages/SeatBooking"));
 
 function App() {
   return (
@@ -19,10 +16,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/vehicle-booking" element={<VehicleBooking />} />
-          {/* <Route path="/about" element={<About />} /> */}
           <Route path="/tours" element={<TourPackagesPage />} />
           <Route path="/tours/:slug" element={<PakagesDetailsPage />} />
-
+          <Route path="/seat-booking" element={<SeatBooking />} />
         </Routes>
       </Suspense>
     </Router>
