5161e5bc21543da96542b3cdb64d96ed91e20ca1
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2025-12-31 22:45:37 +0545

    Wrap PackagesDetails component in MainLayout for consistent page structure

--- clients/adventure nepal/src/pages/PakagesDetailsPage.jsx
+++ clients/adventure nepal/src/pages/PakagesDetailsPage.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import PackagesDetails from "@/components/tour/PakagesDetails"
+import MainLayout from "@/layouts/MainLayout"
 
 const PakagesDetailsPage = () => {
   return (
-    <>
+    <MainLayout>
       <PackagesDetails />
-    </>
+    </MainLayout>
+    
   )
 }
 
