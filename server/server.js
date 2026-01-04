52e45709ad8426fba53c5bdcaa0e71dfec57d7ea
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2026-01-04 23:21:47 +0545

    Initial Express server setup

---
+++ server/server.js
@@ -0,0 +1,11 @@
+
+const express=require("express");
+const cors=require("cors");
+
+const app=express();
+
+require("dotenv").config();
+const port=process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
