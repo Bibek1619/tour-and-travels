06f822c9d2d8d568671f7dfdf6385884c65a28c0
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2026-01-13 21:26:57 +0545

    update main.jsx for tanstack

--- clients/adventure nepal/src/main.jsx
+++ clients/adventure nepal/src/main.jsx
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App.jsx';
 import { Provider } from "react-redux";
 import { store } from "./redux/store"; 
+import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <StrictMode>
+    <QueryClientProvider client={ new QueryClient()}>
+   
       <App />
-    </StrictMode>
+      </QueryClientProvider>
+  
   </Provider>
 );
