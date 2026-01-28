c343bb832407d773bea7e8eb901810b473968b8b
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2026-01-28 17:13:50 +0545

    multiple image uploader multer

---
+++ server/middleware/upload.js
@@ -0,0 +1,21 @@
+// middlewares/upload.js
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const dir = path.join(__dirname, "..", "public", "images"); // ✅ correct path
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
