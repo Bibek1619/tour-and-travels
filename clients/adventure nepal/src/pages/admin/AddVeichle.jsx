e0724340cd30897e9962aa119e68b803103b86a0
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2026-02-06 20:46:45 +0545

    add new

--- clients/adventure nepal/src/pages/admin/AddVeichle.jsx
+++ clients/adventure nepal/src/pages/admin/AddVeichle.jsx
@@ -1,14 +1,19 @@
 import CreateVehicle from "@/components/admin/addVehicle/CreateVehicle";
+import AdminNavbar from "@/components/admin/AdminNavbar";
 
 const AddVehicle =()=>{
     return(
+       < div className=" bg-gray-50 px-4 py-6">
+          <AdminNavbar />   
         <div className="min-h-screen bg-gray-50 px-4 py-6">
+         
 
             <div className="max-w-4xl mx-auto">
                 <h1 className="text-2xl font-semibold mb-6"> Add New Vehicle</h1>
 
                 <CreateVehicle />
             </div>
+        </div>
         </div>
     )
 }
