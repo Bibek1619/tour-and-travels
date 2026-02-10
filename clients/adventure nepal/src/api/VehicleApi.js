53577dac93e0711359998a64df51bf2e7f4fa2dd
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2026-02-10 21:48:20 +0545

    image manage but again error

--- clients/adventure nepal/src/api/VehicleApi.js
+++ clients/adventure nepal/src/api/VehicleApi.js
@@ -4,3 +4,10 @@ export const createVehicleApi = async (formData) => {
   const res = await axiosInstance.post("/vehicles", formData); // formData object
   return res.data;
 };
+
+
+export const getAllVehiclesApi =async ()=>{
+  const res=await axiosInstance.get("/vehicles");
+  return res.data;
+
+}
