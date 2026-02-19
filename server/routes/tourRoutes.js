6f89b99c2690da3f07e45978af2a0fc8fa819625
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2026-02-19 20:28:01 +0545

    route updtate for tour

--- server/routes/tourRoutes.js
+++ server/routes/tourRoutes.js
@@ -1,2 +1,17 @@
-const express=require("express")
-const router=express.router()
+const express = require("express");
+const router = express.Router();
+const {
+  createTourPackage,
+  getAllTourPackages,
+  getTourPackageById,
+  updateTourPackage,
+  deleteTourPackage,
+} = require("../controllers/tourController");
+
+router.post("/", createTourPackage);
+router.get("/", getAllTourPackages);
+router.get("/:id", getTourPackageById);
+router.put("/:id", updateTourPackage);
+router.delete("/:id", deleteTourPackage);
+
+module.exports = router;
