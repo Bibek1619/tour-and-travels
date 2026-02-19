817a9ff49a57cd945e9b512e87e9ee10bb5f1bea
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2026-02-19 20:13:13 +0545

    update controller

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -1,8 +1,118 @@
-const tourPakage =reuire("../models/tourPackage.js")
+const tourPackage =reuire("../models/tourPackage.js")
 
-export.createTourPackage=async(req,res)=>{
+exports.createTourPackage=async (req,res)=>
+{
     try{
-        const{tittle,description,price,duration}=req.body;
-        
+        const tour=await tourPackage.create(req.body);
+        res.status(201).json({
+            sucess:true,
+            message:"Tour Package created successfully",
+            data:tour,
+
+        });
+    
+
+    } catch(error){
+        res.status(500).json({
+            sucess:false,
+            message:"error.message",
+    });
+}
+}
+
+exports.getAllTourPackages=async(req,res)=>{
+    try{
+        const tours=await tourPackage.find().sort({createdAt:-1});
+        res.status(200).json({
+            success:true,
+            count:tours.length,
+            data:tours,
+
+        });
+
+    }catch(error){
+        res.status(500).json({
+            sucess:false,
+            message:error.message,
+
+        })
     }
 }
+
+exports.getTourPackageById=async(req,res)=>{
+    try{
+        const tour=await tourPackage.findById(req.params.id)
+         if(!tour){
+            return res.staus(404).json({
+                sucess:false,
+                message:'tour package not found',
+            });
+         }
+         res.status(200).json({
+      success: true,
+      data: tour,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+    };
+
+
+
+    exports.updateTourPackage=async(req,res)=>{
+        try{
+            const tour=await tourPackage.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                {
+                    new:true,
+                    runValidators:true,
+                }
+            )
+            if(!tour){
+                return res.status(404).json({
+                    success:false,
+                    message:"Tour Package not found",
+                })
+            }
+            res.status(200).json({
+                success:true,
+                message:"Tour Package updated successfully",
+                data:tour,
+            })
+        }catch(error){
+            res.status(500).json({
+                success:false,
+                message:error.message,
+            })
+        }
+    }
+
+
+   exports.deleteTourPackage = async (req, res) => {
+  try {
+    const tour = await TourPackage.findById(req.params.id);
+
+    if (!tour) {
+      return res.status(404).json({
+        success: false,
+        message: "Tour package not found",
+      });
+    }
+
+    await tour.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: "Tour package deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
