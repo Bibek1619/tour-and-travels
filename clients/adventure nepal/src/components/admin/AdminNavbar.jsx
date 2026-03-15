243919b9216b073b1e1e9fe7ea98969b1e80cdd7
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2026-03-15 14:04:06 +0545

    simpelc change for test

--- clients/adventure nepal/src/components/admin/AdminNavbar.jsx
+++ clients/adventure nepal/src/components/admin/AdminNavbar.jsx
@@ -9,14 +9,14 @@ const AdminNavbar = () => {
     <div className="w-full bg-white border-b shadow-sm">
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         {/* Left */}
-        <h1 className="text-xl font-bold text-green-600 ">
+        <h1 className="text-xl font-bold text-green-600  " onClick={()=>navigate("/admin/dashboard")}>
           Admin Dashboard
         </h1>
 
         {/* Right */}
         <div className="flex items-center gap-3">
           <Button
-            onClick={() => navigate("/admin/add-tour")}
+            onClick={() => navigate("/admin/dashboard/add-tour")}
             className="bg-green-500 hover:bg-green-600"
           >
             <Plus size={16} className="mr-1" />
@@ -25,7 +25,7 @@ const AdminNavbar = () => {
 
           <Button
             variant="outline"
-            onClick={() => navigate("/admin/add-vehicle")}
+            onClick={() => navigate("/admin/dashboard/add-vehicle")}
           >
             <Plus size={16} className="mr-1" />
             Add Vehicle Rent
